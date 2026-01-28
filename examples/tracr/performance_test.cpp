c318ceb4b37274825e8199d83591d8cfcd2955c1
noabauma <noah.baumann@h-partners.com>
2026-01-28 15:55:27 +0100

    being able to pass custom marker types to the post processing script

--- examples/tracr/performance_test.cpp
+++ examples/tracr/performance_test.cpp
@@ -27,7 +27,7 @@ int main(void) {
 
   // performance test
   auto perf_test_start = std::chrono::system_clock::now();
-  const int n_sets = 1e6;
+  const int n_sets = 1e2;
   for (int i = 0; i < n_sets; ++i) {
     INSTRUMENTATION_MARK_SET(0, i % 128, 0);
   }
